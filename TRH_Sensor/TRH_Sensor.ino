fc2b598a66ccf1ce031cb21913317a4d80ec5cb5
Zhijie Wang <zjwa127@gmail.com>
2022-06-24 01:49:49 +0800

    Add files via upload

---
+++ TRH_Sensor/TRH_Sensor.ino
@@ -0,0 +1,147 @@
+/**************************************************************************
+  This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+  This example is for a 128x32 pixel display using I2C to communicate
+  3 pins are required to interface (two I2C and one reset).
+
+  Adafruit invests time and resources providing this open
+  source code, please support Adafruit and open-source
+  hardware by purchasing products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries,
+  with contributions from the open source community.
+  BSD license, check license.txt for more information
+  All text above, and the splash screen below must be
+  included in any redistribution.
+ **************************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include "Adafruit_SHT4x.h"
+
+Adafruit_SHT4x sht4 = Adafruit_SHT4x();
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Adafruit SHT4x test");
+  if (! sht4.begin()) {
+    Serial.println("Couldn't find SHT4x");
+    while (1) delay(1);
+  }
+  Serial.println("Found SHT4x sensor");
+  Serial.print("Serial number 0x");
+  Serial.println(sht4.readSerial(), HEX);
+
+  // You can have 3 different precisions, higher precision takes longer
+  sht4.setPrecision(SHT4X_HIGH_PRECISION);
+  switch (sht4.getPrecision()) {
+    case SHT4X_HIGH_PRECISION:
+      Serial.println("High precision");
+      break;
+    case SHT4X_MED_PRECISION:
+      Serial.println("Med precision");
+      break;
+    case SHT4X_LOW_PRECISION:
+      Serial.println("Low precision");
+      break;
+  }
+
+  Serial.println("Found SHT4x sensor");
+  Serial.print("Serial number 0x");
+  Serial.println(sht4.readSerial(), HEX);
+
+  // You can have 3 different precisions, higher precision takes longer
+  sht4.setPrecision(SHT4X_HIGH_PRECISION);
+  switch (sht4.getPrecision()) {
+    case SHT4X_HIGH_PRECISION:
+      Serial.println("High precision");
+      break;
+    case SHT4X_MED_PRECISION:
+      Serial.println("Med precision");
+      break;
+    case SHT4X_LOW_PRECISION:
+      Serial.println("Low precision");
+      break;
+  }
+
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+}
+
+void loop() {
+  sensors_event_t humidity, temp;
+
+  uint32_t timestamp = millis();
+  sht4.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  timestamp = millis() - timestamp;
+
+  Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
+  Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
+
+  Serial.print("Read duration (ms): ");
+  Serial.println(timestamp);
+
+  display.clearDisplay();
+
+  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(5, 5);     // Start at top-left corner
+  //display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.write(("T: " + String(temp.temperature) + " C").c_str());
+  display.setCursor(5, 40);     // Start at top-left corner
+  display.write(("RH:" + String(humidity.relative_humidity) + " %").c_str());
+  display.display();
+  delay(250);
+
+}
+
+
+
+void testdrawchar(void) {
+  display.clearDisplay();
+
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.write("temeprature");
+  display.display();
+  delay(2000);
+}
+
+
+
+
+#define XPOS   0 // Indexes into the 'icons' array in function below
+#define YPOS   1
+#define DELTAY 2
