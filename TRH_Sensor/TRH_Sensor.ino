f289ae66583a8331606567d20848949226f4d323
Zhijie Wang <zjwa127@gmail.com>
2022-07-06 13:48:52 +0800

    Update TRH_Sensor.ino

--- TRH_Sensor/TRH_Sensor.ino
+++ TRH_Sensor/TRH_Sensor.ino
@@ -1,23 +1,3 @@
-/**************************************************************************
-  This is an example for our Monochrome OLEDs based on SSD1306 drivers
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/category/63_98
-
-  This example is for a 128x32 pixel display using I2C to communicate
-  3 pins are required to interface (two I2C and one reset).
-
-  Adafruit invests time and resources providing this open
-  source code, please support Adafruit and open-source
-  hardware by purchasing products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries,
-  with contributions from the open source community.
-  BSD license, check license.txt for more information
-  All text above, and the splash screen below must be
-  included in any redistribution.
- **************************************************************************/
-
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -48,6 +28,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define NUMFLAKES     10 // Number of snowflakes in the animation example
 
+float tem, hum;
 
 void setup() {
   Serial.begin(115200);
@@ -103,8 +84,6 @@ void setup() {
 
   // Show initial display buffer contents on the screen --
   // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(2000); // Pause for 2 seconds
 
   // Clear the buffer
   display.clearDisplay();
@@ -125,22 +104,24 @@ void loop() {
   Serial.println(timestamp);
 
   display.clearDisplay();
+  tem = temp.temperature;
+  hum = humidity.relative_humidity;
 
+  
   display.setTextSize(2);      // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE); // Draw white text
   display.setCursor(5, 10);     // Start at top-left corner
   //display.cp437(true);         // Use full 256 char 'Code Page 437' font
-  display.write(("T: " + String(temp.temperature) + " C").c_str());
+  display.write(("T: " + String(tem,1) + " C").c_str());
   display.setCursor(5, 40);     // Start at top-left corner
-  display.write(("RH:" + String(humidity.relative_humidity) + " %").c_str());
+  display.write(("RH:" + String(hum,1) + " %").c_str());
   display.display();
-  SerialBT.print(("T: " + String(temp.temperature) + " C, ").c_str());
-  SerialBT.print(("RH:" + String(humidity.relative_humidity) + " %, \n").c_str());
+  SerialBT.print(("T: " + String(tem,1) + " C, ").c_str());
+  SerialBT.print(("RH:" + String(hum,1) + " %, \n").c_str());
 
   delay(1000);
 
 }
-
 
 
 void testdrawchar(void) {
@@ -154,7 +135,6 @@ void testdrawchar(void) {
   display.display();
   delay(2000);
 }
-
 
 
 
