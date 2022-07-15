8d1fd4390e16e4741ae801d03c40304d5d84291a
Zhijie Wang <zjwa127@gmail.com>
2022-07-15 10:44:40 +0800

    Update SHT4x_TMP_Batt_SS.ino

--- SHT4x_TMP_Batt_SS/SHT4x_TMP_Batt_SS.ino
+++ SHT4x_TMP_Batt_SS/SHT4x_TMP_Batt_SS.ino
@@ -12,14 +12,15 @@
 #error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
 #endif
 #include <movingAvg.h>                  // https://github.com/JChristensen/movingAvg
-movingAvg avgTemp(15);                  // define the moving average object
+movingAvg avgTemp(10);                  // define the moving average object
+movingAvg avgbatt(10);                  // define the moving average object
 
 BluetoothSerial SerialBT;
-int maxV = 3900    /1.333;
-int minV = 2279    /1.333;
+int maxV = 3900    / 1.333;
+int minV = 2279    / 1.333;
 #include "Adafruit_SHT4x.h"
 
-Adafruit_SHT4x sht4 = Adafruit_SHT4x();
+//Adafruit_SHT4x sht4 = Adafruit_SHT4x();
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -34,47 +35,22 @@ void setup() {
   Serial.begin(115200);
   SerialBT.begin("ESP32test"); //Bluetooth device name
   avgTemp.begin();
-
-  Serial.println("Adafruit SHT4x test");
-  if (! sht4.begin()) {
-    Serial.println("Couldn't find SHT4x");
-    while (1) delay(1);
-  }
-  Serial.println("Found SHT4x sensor");
-  Serial.print("Serial number 0x");
-  Serial.println(sht4.readSerial(), HEX);
-
-  // You can have 3 different precisions, higher precision takes longer
-  sht4.setPrecision(SHT4X_HIGH_PRECISION);
-  switch (sht4.getPrecision()) {
-    case SHT4X_HIGH_PRECISION:
-      Serial.println("High precision");
-      break;
-    case SHT4X_MED_PRECISION:
-      Serial.println("Med precision");
-      break;
-    case SHT4X_LOW_PRECISION:
-      Serial.println("Low precision");
-      break;
-  }
-
-  Serial.println("Found SHT4x sensor");
-  Serial.print("Serial number 0x");
-  Serial.println(sht4.readSerial(), HEX);
-
-  // You can have 3 different precisions, higher precision takes longer
-  sht4.setPrecision(SHT4X_HIGH_PRECISION);
-  switch (sht4.getPrecision()) {
-    case SHT4X_HIGH_PRECISION:
-      Serial.println("High precision");
-      break;
-    case SHT4X_MED_PRECISION:
-      Serial.println("Med precision");
-      break;
-    case SHT4X_LOW_PRECISION:
-      Serial.println("Low precision");
-      break;
-  }
+  avgbatt.begin();
+//  Serial.println("Adafruit SHT4x test");
+//  if (! sht4.begin()) {
+//    Serial.println("Couldn't find SHT4x");
+//    while (1) delay(1);
+//  }
+//  Serial.println("Found SHT4x sensor");
+//  Serial.print("Serial number 0x");
+//  Serial.println(sht4.readSerial(), HEX);
+//
+//  // You can have 3 different precisions, higher precision takes longer
+//  sht4.setPrecision(SHT4X_HIGH_PRECISION);
+//  Serial.println("Found SHT4x sensor");
+//  Serial.print("Serial number 0x");
+//  Serial.println(sht4.readSerial(), HEX);
+//  sht4.setPrecision(SHT4X_HIGH_PRECISION);
 
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
@@ -92,42 +68,38 @@ void setup() {
 }
 
 void loop() {
-  sensors_event_t humidity, temp;
+//  sensors_event_t humidity, temp;
 
-  uint32_t timestamp = millis();
-  sht4.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
-  timestamp = millis() - timestamp;
+//  sht4.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
 
 
-
-  Serial.print("Read duration (ms): ");
-  Serial.println(timestamp);
 
   display.clearDisplay();
-  tem = temp.temperature;
-  hum = humidity.relative_humidity;
+//  tem = temp.temperature;
+//  hum = humidity.relative_humidity;
   int temp_a = analogRead(2);
-  float T_temp = (temp_a * 0.02614 - 1.31345)*10;
+  float T_temp = (temp_a * 0.02614 - 1.31345) * 10;
   float avg = avgTemp.reading(T_temp) / 10.0;
-  int batt_a = analogRead(4);
-  int batt_b = batt_a*3300/4095;
-  float batt = (batt_b-minV)/((maxV-minV)/100);
-  if (batt > 100){
+  int batt_a = avgbatt.reading(analogRead(4));
+  int batt_b = batt_a * 3300 / 4095;
+  float batt = (batt_b - minV) / ((maxV - minV) / 100);
+  if (batt > 100) {
     batt = 100;
   }
-   if (batt < 0){
+  if (batt < 0) {
     batt = 0;
   }
   display.setTextSize(0.5);      // Normal 1:1 pixel scale
-  
-  //Serial Monitor
-  Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
-  Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
-  Serial.print("TMP: "); Serial.print(avg); Serial.println(" C.");
-  Serial.print("Battery: "); Serial.print(batt); Serial.println("%");
 
+  //Serial Monitor
+//  Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
+//  Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
+  Serial.print("TMST: "); Serial.print(avg); Serial.print(" C.");Serial.print(" V_T: "); Serial.print(temp_a * 3.3 / 4.095); Serial.println(" mV");
+  Serial.print("T_AnalogRead: "); Serial.print(temp_a); Serial.println(" count");
+  Serial.print("Battery: "); Serial.print(batt); Serial.print("%");Serial.print(" V_Batt: "); Serial.print(batt_b); Serial.println(" mV");
+  Serial.println(" ");
   //Display
-   display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setTextColor(SSD1306_WHITE); // Draw white text
   display.setCursor(5 , 5);    // Start at top-left corner
   display.write(("Bat: " + String(batt, 0) + " %").c_str());
   display.setCursor(70 , 5);    // Start at top-left corner
@@ -136,14 +108,14 @@ void loop() {
   display.write(("TMP: " + String(avg) + " C").c_str());
   display.setCursor(5, 35);     // Start at top-left corner
   //display.cp437(true);         // Use full 256 char 'Code Page 437' font
-  display.write(("T: " + String(tem, 1) + " C").c_str());
-  display.setCursor(5, 50);     // Start at top-left corner
-  display.write(("RH:" + String(hum, 1) + " %").c_str());
+//  display.write(("T: " + String(tem, 1) + " C").c_str());
+//  display.setCursor(5, 50);     // Start at top-left corner
+//  display.write(("RH:" + String(hum, 1) + " %").c_str());
   display.display();
-  
+
   //BT
   SerialBT.print(("T: " + String(avg, 1) + " C, ").c_str());
-  SerialBT.print(("RH:" + String(hum, 1) + " %, \n").c_str());
+//  SerialBT.print(("RH:" + String(hum, 1) + " %, \n").c_str());
 
   delay(1000);
 
